9dc4a8b9449df159e5b9ff5d43433cc82ce8ac31
Swastik Supakar <69300297+swastik58@users.noreply.github.com>
2022-01-05 11:39:37 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: 'ShareMe',
-    description: "za social media application where users can share images with on a feed page andcan also download and save other's images",
+    description: "A social media application where users can share images with on a feed page and can also download and save other's images",
     image: '/images/9.png',
     tags: ['Sanity', 'UUID',  'Masonry Layout', 'React Icons'],
     source: 'https://github.com/swastik58/share_me',
